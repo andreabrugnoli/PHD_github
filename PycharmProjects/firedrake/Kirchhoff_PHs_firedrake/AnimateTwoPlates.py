1691c3ad8378f3a6f1ad02a2c63913901329f67a
brugnoss <brugnoss.ab@gmail.com>
2019-03-19 18:43:38 -0300

    Interconnection of two Kirchhoff plates

---
+++ PycharmProjects/firedrake/Kirchhoff_PHs_firedrake/AnimateTwoPlates.py
@@ -0,0 +1,61 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d import Axes3D
+
+from firedrake.plot import _two_dimension_triangle_func_val
+
+matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams["legend.loc"] = 'upper right'
+
+def animate2D(minSol, maxSol, solPl1_list, solPl2_list, t, xlabel = None, ylabel = None,  zlabel = None, title = None):
+    tol = 1e-4
+    fntsize = 20
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    def update_plot(frame_number, solPl1_list, solPl2_list, plot):
+        ax.collections.clear()
+        lab = 'Time =' + '{0:.2e}'.format(t[frame_number])
+        triangulation1, Z1 = _two_dimension_triangle_func_val(solPl1_list[frame_number], 10)
+        plot = ax.plot_trisurf(triangulation1, Z1, label=lab, **surf_opts1)
+        plot._facecolors2d = plot._facecolors3d
+        plot._edgecolors2d = plot._edgecolors3d
+
+        triangulation2, Z2 = _two_dimension_triangle_func_val(solPl2_list[frame_number], 10)
+        ax.plot_trisurf(triangulation2, Z2, **surf_opts2)
+
+    ax.set_xlabel(xlabel, fontsize=fntsize)
+    ax.set_ylabel(ylabel, fontsize=fntsize)
+    ax.set_zlabel(zlabel, fontsize=fntsize)
+
+    if minSol == maxSol:
+        raise ValueError('Constant function for drawnow')
+
+    ax.set_xlabel(xlabel, fontsize=fntsize)
+    ax.set_ylabel(ylabel, fontsize=fntsize)
+    ax.set_zlabel(zlabel, fontsize=fntsize)
+
+    ax.set_zlim(minSol - 1e-3 * abs(minSol), maxSol + 1e-3 * abs(maxSol))
+    ax.w_zaxis.set_major_locator(LinearLocator(10))
+    ax.w_zaxis.set_major_formatter(FormatStrFormatter('%1.2g'))
+
+    ax.set_title(title, fontsize=fntsize, loc='left')
+
+    lab = 'Time =' + '{0:.2e}'.format(t[0])
+
+    surf_opts1 = {'cmap': cm.winter, 'linewidth': 0, 'antialiased': False, 'vmin': minSol, 'vmax': maxSol}
+    surf_opts2 = {'cmap': cm.inferno, 'linewidth': 0, 'antialiased': False, 'vmin': minSol, 'vmax': maxSol}
+
+    triangulation1, Z1 = _two_dimension_triangle_func_val(solPl1_list[0], 10)
+    plot = ax.plot_trisurf(triangulation1, Z1, label=lab, **surf_opts1)
+
+    triangulation2, Z2 = _two_dimension_triangle_func_val(solPl2_list[0], 10)
+    ax.plot_trisurf(triangulation2, Z2, label=lab, **surf_opts2)
+
+    anim = animation.FuncAnimation(fig, update_plot, frames=len(t),\
+                                    interval = 10, fargs=(solPl1_list, solPl2_list, plot))
+    return anim
--- PycharmProjects/firedrake/Kirchhoff_PHs_firedrake/TwoPlateInt.py
+++ PycharmProjects/firedrake/Kirchhoff_PHs_firedrake/TwoPlateInt.py
@@ -4,6 +4,8 @@ import numpy as np
 np.set_printoptions(threshold=np.inf)
 from math import floor, sqrt, pi
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
 
 from scipy import linalg as la
 from scipy.sparse import csc_matrix
@@ -16,7 +18,7 @@ h = 0.05 # 0.01
 rho = 2700  # kg/m^3
 D = E * h ** 3 / (1 - nu ** 2) / 12.
 
-n = 3
+n = 4
 
 
 D_b = as_tensor([
@@ -132,25 +134,191 @@ R_pl = np.zeros((n_pl, n_pl))
 n = FacetNormal(mesh)
 # s = as_vector([-n[1], n[0]])
 
-V_qn = FunctionSpace(mesh, 'Lagrange', 2)
-V_Mnn = FunctionSpace(mesh, 'Lagrange', 2)
+V_qn = FunctionSpace(mesh, 'Lagrange', 1)
+V_Mnn = FunctionSpace(mesh, 'Lagrange', 1)
 
 Vu = V_qn * V_Mnn
 
 q_n, M_nn = TrialFunction(Vu)
-
 v_omn = dot(grad(v_p), n)
-b_l = v_p * q_n * ds(1) + v_omn * M_nn * ds(1)
-# b_r = v_p * q_n * ds(2)
+g_l = v_p * q_n * ds(1) + v_omn * M_nn * ds(1)
+g = v_p * q_n * ds + v_omn * M_nn * ds
 
 # Assemble the stiffness matrix and the mass matrix.
 
-B = assemble(b_l,  mat_type='aij')
-petsc_b = B.M.handle
+petsc_g_l = assemble(g_l,  mat_type='aij').M.handle
+petsc_g = assemble(g,  mat_type='aij').M.handle
 
-G_pl = np.array(petsc_b.convert("dense").getDenseArray())
+G_l = np.array(petsc_g_l.convert("dense").getDenseArray())
 
-boundary_dofs = np.where(G_pl.any(axis=0))[0]
-G_pl = G_pl[:,boundary_dofs]
+bd_l = np.where(G_l.any(axis=0))[0]
 
-n_mul = len(boundary_dofs)
+G_clamp = G_l[:, bd_l]
+
+n_mul = len(bd_l)
+
+G_all = np.array(petsc_g.convert("dense").getDenseArray())
+
+tab_coord = mesh.coordinates.dat.data
+
+tab_x = tab_coord[:, 0]
+tab_y = tab_coord[:, 1]
+
+
+dof_int_l = ((tab_x == l_x) & (tab_y == l_y/2)).nonzero()[0][0]
+dof_int_r = ((tab_x == 0.0) & (tab_y == l_y/2)).nonzero()[0][0]
+
+n_qn = V_qn.dim()
+Gint_pl1 = G_all[:, (dof_int_l, dof_int_l + n_qn)]
+
+Gint_pl2 = G_all[:, (dof_int_r, dof_int_r + n_qn)]
+
+Gint = np.concatenate((Gint_pl1, -Gint_pl2), axis=0)
+
+G_D = np.concatenate((G_clamp, np.zeros((n_pl, n_mul)) ), axis=0)
+
+G = np.concatenate((G_D, Gint), axis=1)
+
+x, y = SpatialCoordinate(mesh)
+A = Constant(10**5)
+f_w = project(A*y, Vp)
+b_f1 = v_p * f_w * dx  # ds(3) - v_pw * f_w *  ds(4)
+Bf_pl1 = assemble(b_f1, mat_type='aij').vector().get_local()
+
+b_f2 = - v_p * f_w * dx  # ds(3) - v_pw * f_w *  ds(4)
+Bf_pl2 = assemble(b_f2, mat_type='aij').vector().get_local()
+
+Mint = la.block_diag(M_pl, M_pl)
+Jint = la.block_diag(J_pl, J_pl)
+Rint = la.block_diag(R_pl, R_pl)
+
+Bf_int = np.concatenate((Bf_pl1, Bf_pl2), axis=0)
+
+Mint_sp = csc_matrix(Mint)
+invMint = inv_sp(Mint_sp)
+invMint = invMint.toarray()
+
+S = G @ la.inv(G.T @ invMint @ G) @ G.T @ invMint
+
+n_tot = 2 * n_pl
+I = np.eye(n_tot)
+
+P = I - S
+
+Jsys = P @ Jint
+Rsys = P @ Rint
+Fsys = P @ Bf_int
+
+
+t0 = 0.0
+t_fin = 0.001
+n_t = 100
+t_span = [t0, t_fin]
+
+def sys(t,y):
+    if t< 0.2 * t_fin:
+        bool_f = 1
+    else: bool_f = 0
+    dydt = invMint @ ( (Jsys - Rsys) @ y + Fsys *bool_f)
+    return dydt
+
+
+y0 = np.zeros(n_tot,)
+# y0[:n_pl] = e_pl0.vector().get_local()
+
+t_ev = np.linspace(t0, t_fin, num = n_t)
+
+sol = integrate.solve_ivp(sys, t_span, y0, method='RK45', vectorized=False, t_eval = t_ev)
+
+t_ev = sol.t
+n_ev = len(t_ev)
+dt_vec = np.diff(t_ev)
+
+e_pl1 = sol.y[:n_pl, :]
+e_rod = sol.y[n_pl: n_tot, :]
+
+
+e_pl1 = sol.y[:n_pl, :]
+e_pl2 = sol.y[n_pl:n_tot, :]
+
+e_pw1 = e_pl1[:n_Vp, :]  # np.zeros((n_pw,n_t))
+e_pw2 = e_pl2[:n_Vp, :]
+
+w0_pl1 = np.zeros((n_Vp,))
+w_pl1 = np.zeros(e_pw1.shape)
+w_pl1[:, 0] = w0_pl1
+w_pl1_old = w_pl1[:, 0]
+
+w0_pl2 = np.zeros((n_Vp,))
+w_pl2 = np.zeros(e_pw2.shape)
+w_pl2[:, 0] = w0_pl2
+w_pl2_old = w_pl2[:, 0]
+
+dt_vec = np.diff(t_ev)
+for i in range(1, n_ev):
+    w_pl1[:, i] = w_pl1_old + 0.5 * (e_pw1[:, i - 1] + e_pw1[:, i]) * dt_vec[i-1]
+    w_pl1_old = w_pl1[:, i]
+
+    w_pl2[:, i] = w_pl2_old + 0.5 * (e_pw2[:, i - 1] + e_pw2[:, i]) * dt_vec[i-1]
+    w_pl2_old = w_pl2[:, i]
+
+
+w_pl1_mm = w_pl1 * 1000
+w_pl2_mm = w_pl2 * 1000
+
+wmm_pl1_CGvec = []
+wmm_pl2_CGvec = []
+
+w_fun = Function(Vp)
+Vp_CG = FunctionSpace(mesh, 'Lagrange', 3)
+n_VpCG = Vp_CG.dim()
+print(n_Vp, n_VpCG)
+
+maxZvec = np.zeros(n_ev)
+minZvec = np.zeros(n_ev)
+for i in range(n_ev):
+    w_fun.vector()[:] = w_pl1_mm[:, i]
+    wmm_pl1_CG = project(w_fun, Vp_CG)
+    wmm_pl1_CGvec.append(wmm_pl1_CG)
+
+    w_fun.vector()[:] = w_pl2_mm[:, i]
+    wmm_pl2_CG = project(w_fun, Vp_CG)
+    wmm_pl2_CGvec.append(wmm_pl2_CG)
+
+    maxZvec[i] = max(max(wmm_pl1_CG.vector()), max(wmm_pl2_CG.vector()))
+    minZvec[i] = min(min(wmm_pl1_CG.vector()), min(wmm_pl2_CG.vector()))
+
+maxZ = max(maxZvec)
+minZ = min(minZvec)
+
+
+Hpl1_vec = np.zeros((n_ev,))
+Hpl2_vec = np.zeros((n_ev,))
+
+for i in range(n_ev):
+    Hpl1_vec[i] = 0.5 * (e_pl1[:, i].T @ M_pl @ e_pl1[:, i])
+    Hpl2_vec[i] = 0.5 * (e_pl2[:, i].T @ M_pl @ e_pl2[:, i])
+
+fig = plt.figure()
+plt.plot(t_ev, Hpl1_vec, 'b-', label='Hamiltonian Plate 1 (J)')
+plt.plot(t_ev, Hpl2_vec, 'r-', label='Hamiltonian Plate 2 (J)')
+plt.plot(t_ev, Hpl1_vec + Hpl2_vec, 'g-', label='Total Energy (J)')
+plt.xlabel(r'{Time} (s)', fontsize=16)
+plt.ylabel(r'{Hamiltonian} (J)', fontsize=16)
+plt.title(r"Hamiltonian trend",
+          fontsize=16)
+plt.legend(loc='upper left')
+
+from AnimateTwoPlates import animate2D
+import matplotlib.animation as animation
+sol = np.concatenate( (w_pl1_mm, w_pl2_mm), axis=0)
+anim = animate2D(minZ, maxZ, wmm_pl1_CGvec, wmm_pl2_CGvec, t_ev, xlabel = '$x[m]$', ylabel = '$y [m]$', \
+                         zlabel = '$w [mm]$', title = 'Vertical Displacement')
+
+# Writer = animation.writers['ffmpeg']
+# writer = Writer(fps=20, metadata=dict(artist='Me'), bitrate=1800)
+#
+# anim.save('IntPlates.mp4', writer=writer)
+
+
+plt.show()
