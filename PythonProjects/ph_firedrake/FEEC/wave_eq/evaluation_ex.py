ce9993f8db7576d7080a9c4ccdea94be97fd18b6
andreabrugnoli <brugnoss.ab@gmail.com>
2021-09-10 13:57:36 +0200

    scava

--- PythonProjects/ph_firedrake/FEEC/wave_eq/evaluation_ex.py
+++ PythonProjects/ph_firedrake/FEEC/wave_eq/evaluation_ex.py
@@ -25,7 +25,7 @@ phi_x = 1
 phi_y = 10
 phi_t = 5
 
-# w_ex = sin(om_x * x + phi_x) * sin(om_y * y + phi_y) * sin(om_t * t + phi_t)
+w_ex = sin(om_x * x + phi_x) * sin(om_y * y + phi_y) * sin(om_t * t + phi_t)
 p_ex = om_t * sin(om_x * x + phi_x) * sin(om_y * y + phi_y) * cos(om_t * t + phi_t)
 u_ex = as_vector([om_x * cos(om_x * x + phi_x) * sin(om_y * y + phi_y) * sin(om_t * t + phi_t),
                   om_y * sin(om_x * x + phi_x) * cos(om_y * y + phi_y) * sin(om_t * t + phi_t)])
@@ -34,7 +34,9 @@ bdflow_ex_n1 = dot(u_ex, n_ver) * ds(domain=mesh)
 bdflow_ex_n2 = p_ex * ds(domain=mesh)
 bdflow_ex_n3 = p_ex * dot(u_ex, n_ver) * ds(domain=mesh)
 
-Hn_ex = 0.5 * (inner(p_ex, p_ex) * dx(domain=mesh) + inner(u_ex, u_ex) * dx(domain=mesh))
+H_ex_n1 = 0.5 * p_ex*2 *dx(domain=mesh) + 0.5*dot(u_ex, u_ex) * dx(domain=mesh)
+H_ex_n2 = 0.5 * p_ex*2 *dx(domain=mesh) + 0.5*dot(grad(w_ex), grad(w_ex)) * dx(domain=mesh)
+
 
 
 for ii in range(10):
@@ -44,8 +46,6 @@ for ii in range(10):
     bd_flow2 = assemble(bdflow_ex_n2)
     bd_flow3 = assemble(bdflow_ex_n3)
 
-    Hn = assemble(Hn_ex)
-
     print("Boundary flow 1:")
     print(bd_flow1)
     print("Boundary flow 2:")
@@ -53,5 +53,12 @@ for ii in range(10):
     print("Boundary flow 3:")
     print(bd_flow3)
 
-    print("Energy")
-    print(Hn)
+for ii in range(10):
+    t.assign(ii)
+    H_n1 = assemble(H_ex_n1)
+    H_n2 = assemble(H_ex_n2)
+
+    print("Energy1")
+    print(H_n1)
+    print("Energy2")
+    print(H_n2)
